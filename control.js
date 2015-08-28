43bd94ad01931dffd337b06b571363dce78de8f0
hyunghunny <hyunghunny@gmail.com>
2015-08-28 17:25:03 +0900

    add sensorchart emitting

---
+++ control.js
@@ -0,0 +1,68 @@
+var people_counter = require("./pc.js");
+var dbMgr = require("./dbmanager.js");
+var sensorchart = require("http://adsl.snu.ac.kr:3000/api.js");
+
+exports.run = function () {
+	// initialize DB manager
+	dbMgr.setTable('adsl1');
+
+	var gpioPin1 = 16; // #23
+	var gpioPin2 = 18; // #24
+
+	var intervalTime = 0;
+
+	people_counter.cleanup(gpioPin1, gpioPin2); // release ports if it was used.
+
+	// XXX: this code is not working properly.
+	process.on('exit', function (code) {
+		// close ports when it exits
+		people_counter.cleanup(gpioPin1, gpioPin2);
+	});
+
+	people_counter.initialize(gpioPin1, gpioPin2, function() {
+			console.log('ready to read sensors');
+			readSignalsInfinite();
+		},
+		function () {
+			console.log('failed to initialized');
+	});
+}
+
+function readSignalsInfinite() {
+	setInterval(function() {
+		var ports = [gpioPin1, gpioPin2];
+		//console.log('try to read sensor values...');
+		for (var i = 0; i < ports.length; i++) {
+			//console.log('try to read value at ' + ports[i]);
+			people_counter.readValuesAsync(ports[i], function (timestamp, value) {
+				dbMgr.save(timestamp, value);
+				transmit(timestamp, value); // XXX:add for sensor chart testing
+			});
+		}
+	}
+	, intervalTime);
+}
+
+function transmit(timestamp, value) {
+
+	var obs = {
+		"datePublished": timestamp,
+		"value" : value
+	}
+	var observations = [ obs ];
+  var id = 'webofthink';
+	var password = '';
+	var sensorUid = 22; // people counter
+
+	sensorchart.login(id, password, function (transmitter) {
+	    if (transmitter) {
+	        transmitter.emit(sensorId, observations, function (result) {
+	            if (result == false) {
+	                console.log('failed to transmit observations.');
+	            }
+	        });
+	    } else {
+	        console.log('login error!');
+	    }
+	});
+}
