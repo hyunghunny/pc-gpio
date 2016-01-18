a1dffbaeb18e98d18d553e3a1e2be5c68633cd79
hyunghunny <hyunghunny@gmail.com>
2016-01-18 22:45:05 +0900

    fix no response bug

--- control.js
+++ control.js
@@ -15,12 +15,6 @@ exports.run = function () {
 
 	//people_counter.cleanup(gpioPin1, gpioPin2); // release ports if it was used.
 
-	// XXX: this code is not working properly.
-	process.on('exit', function (code) {
-		// close ports when it exits
-		people_counter.cleanup(gpioPin1, gpioPin2);
-	});
-
 	people_counter.initialize(gpioPin1, gpioPin2,
 		function() {
 			console.log('ready to read sensors');
@@ -46,12 +40,26 @@ function readSignalsInfinite() {
 				transmit(timestamp, value); // XXX:add for sensor chart testing
 			});
 		}
-		// try to keep connecting
-		sensorchart.login(id, password, function (obj) {
-			transmitter = obj;
-		});
+
 	}, intervalTime);
 }
+
+// try to keep connecting
+setInterval(function() {
+	var ports = [gpioPin1, gpioPin2];
+	//console.log('try to read sensor values...');
+	for (var i = 0; i < ports.length; i++) {
+		//console.log('try to read value at ' + ports[i]);
+		people_counter.readValuesAsync(ports[i], function (timestamp, value) {
+			dbMgr.save(timestamp, value);
+			transmit(timestamp, value); // XXX:add for sensor chart testing
+		});
+	}
+	sensorchart.login(id, password, function (obj) {
+		console.log('success to login');
+		transmitter = obj;
+	});
+}, 60000);
 
 var observations = [];
 function transmit(timestamp, value) {
